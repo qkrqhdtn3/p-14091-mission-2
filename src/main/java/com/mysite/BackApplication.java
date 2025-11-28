1963722536030924e9bcefa0057a3423264a7253
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-28 16:41:20 +0900

    chore: rename package to com.mysite

--- src/main/java/com/mysite/BackApplication.java
+++ src/main/java/com/mysite/BackApplication.java
@@ -1,4 +1,4 @@
-package com.back;
+package com.mysite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mysite/BackApplicationTests.java
+++ src/test/java/com/mysite/BackApplicationTests.java
@@ -1,4 +1,4 @@
-package com.back;
+package com.mysite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
