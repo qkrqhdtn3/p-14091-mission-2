e039f591b8d77392d6ab1f82d85453fe916c8e33
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-29 22:57:35 +0900

    test: add tests for findBySubjectLike method

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -52,10 +52,17 @@ class SbbApplicationTests {
 //        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요");
 //        assertEquals(1, q.getId());
 //    }
+//
+//    @Test
+//    void testJpa5(){
+//        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요", "sbb에 대해서 알고 싶습니다");
+//        assertEquals(1, q.getId());
+//    }
 
     @Test
-    void testJpa5(){
-        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요", "sbb에 대해서 알고 싶습니다");
-        assertEquals(1, q.getId());
+    void testJpa6(){
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요", q.getSubject());
     }
 }
