eed67a6c2756d29405c80b549b0984e395605c38
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-29 21:57:10 +0900

    test: add questionRepository save test

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -8,21 +8,21 @@ import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
-//    @Autowired
-//    private QuestionRepository questionRepository;
-//
-//	@Test
-//	void testJpa() {
-//        Question question1 = new Question();
-//        question1.setSubject("sbb가 무엇인가요");
-//        question1.setContent("sbb에 대해서 알고 싶습니다");
-//        question1.setCreateDate(LocalDateTime.now());
-//        this.questionRepository.save(question1);
-//
-//        Question question2 = new Question();
-//        question2.setSubject("스프링부트 모델 질문");
-//        question2.setContent("id는 자동 생성 되나요?");
-//        question2.setCreateDate(LocalDateTime.now());
-//        this.questionRepository.save(question2);
-//	}
+    @Autowired
+    private QuestionRepository questionRepository;
+
+	@Test
+	void testJpa() {
+        Question question1 = new Question();
+        question1.setSubject("sbb가 무엇인가요");
+        question1.setContent("sbb에 대해서 알고 싶습니다");
+        question1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question1);
+
+        Question question2 = new Question();
+        question2.setSubject("스프링부트 모델 질문");
+        question2.setContent("id는 자동 생성 되나요?");
+        question2.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question2);
+	}
 }
