37529c733049802bd6878c3fd6d5a70b9621e645
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-30 03:12:01 +0900

    feat: add AnswerController with /answer/create/{id} endpoint

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @GetMapping("/create/{id}")
+    public String createAnswer(Model model,
+                               @PathVariable Integer id,
+                               @RequestParam(value="content") String content){
+        Question question = this.questionService.getQuestion(id);
+//        TODO: 답변 저장
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
