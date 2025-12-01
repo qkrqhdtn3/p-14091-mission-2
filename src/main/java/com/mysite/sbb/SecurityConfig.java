d0a6ff2e7544ff81a1eb459268c0be470b1cccb6
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-01 17:38:53 +0900

    feat: add basic spring security configuration

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.AntPathMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests((authorizeHttpRequests)
+                -> authorizeHttpRequests
+                .requestMatchers("/**").permitAll());
+        return httpSecurity.build();
+    }
+}
