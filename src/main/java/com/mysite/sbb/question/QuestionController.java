4c49606e12bb339d0d738a3d187d40317c636d00
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-02 01:17:59 +0900

    feat: add question modification functionality to controller

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -71,5 +72,20 @@ public class QuestionController {
         return "question_form";
     }
 
-
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm,
+                                 @PathVariable("id") Integer id,
+                                 BindingResult bindingResult,
+                                 Principal principal){
+        if(bindingResult.hasErrors()){
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
 }
